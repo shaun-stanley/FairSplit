f4082043dad2075c97d45ed24d4df2019d5ea2ba
Shaun Stanley <shaunstanley.dev@gmail.com>
2025-08-24 07:19:42 +0530

    Initial Commit

---
+++ FairSplit/FairSplitApp.swift
@@ -0,0 +1,32 @@
+//
+//  FairSplitApp.swift
+//  FairSplit
+//
+//  Created by Shaun Stanley on 8/24/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FairSplitApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FairSplitTests/FairSplitTests.swift
@@ -0,0 +1,17 @@
+//
+//  FairSplitTests.swift
+//  FairSplitTests
+//
+//  Created by Shaun Stanley on 8/24/25.
+//
+
+import Testing
+@testable import FairSplit
+
+struct FairSplitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
